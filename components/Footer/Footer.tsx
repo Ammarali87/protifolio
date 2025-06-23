1a80a78a2a18cebc01fd0e670c92454d6417a6e4
AmmarAli87 <amarhero501@gmail.com>
2025-06-23 23:36:05 +0300

    welcome

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,8 +3,13 @@
 export default function Footer() {
   return (
    
-      <div className=" w-full items-center bg-gray-100 ">
+      <div className=" w-full relative items-center bg-gray-100 ">
         {/* <h1 className="bg-red-500  p-4 text-center">Footer </h1> */}
+        <p className="bg-blue-400/80 rounded-lg  text-sm absolute bottom-8 left-1/3 ms-0 md:ms-11 p-2 px-3"> All right reserved by
+          <span className=" text-md ms-2 font-bold ">
+          Ammar Ali 
+          </span>
+         </p>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1"
          d="M0,128L40,144C80,160,160,192,240,186.7C320,181,400,139,480,112C560,85,640,75,720,85.3C800,96,880,128,960,144C1040,160,1120,160,1200,165.3C1280,171,1360,181,1400,186.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
       </div>
